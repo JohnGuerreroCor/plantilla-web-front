a05215e6a048e3cbf2216b25c013a0f5c58640b2
JohnGuerreroCor <john.guerrero@usco.edu.co>
2024-09-09 16:38:30 -0500

    Ajustes plantillas

--- src/app/services/foto.service.ts
+++ src/app/services/foto.service.ts
@@ -13,11 +13,6 @@ export class FotoService {
   private url: string = `${environment.URL_BACKEND}/foto`; // URL base para las operaciones del servicio
   private httpHeaders = new HttpHeaders(); // Encabezados HTTP para las solicitudes
 
-  // Obtiene información del usuario del servicio de autenticación
-  private uaa = this.authservice.obtenerUaa();
-  private perCodigo = this.authservice.obtenerPerCodigo();
-  userLogeado: String = this.authservice.user.username; // Nombre de usuario logeado
-
   constructor(
     private http: HttpClient, // Módulo para realizar solicitudes HTTP
     private router: Router, // Servicio de enrutamiento
@@ -45,29 +40,18 @@ export class FotoService {
     return false;
   }
 
-  // Método para subir una foto al servidor
-  subirFoto(archivo: File): Observable<null> {
-    let formData: FormData = new FormData();
-    formData.set('foto', archivo);
-    return this.http.post<null>(
-      `${this.url}/subir/${this.userLogeado}/${this.perCodigo}/${this.uaa}`,
-      formData,
-      { headers: this.aggAutorizacionHeader() }
-    );
-  }
-
   // Método para obtener la foto del usuario
   mirarFoto(perCodigo: String): Observable<any> {
-    return this.http.get<any>(
-      `${this.url}/obtener-foto/${this.userLogeado}/${perCodigo}`,
-      { headers: this.aggAutorizacionHeader(), responseType: 'blob' as 'json' }
-    );
+    return this.http.get<any>(`${this.url}/obtener-foto/${perCodigo}`, {
+      headers: this.aggAutorizacionHeader(),
+      responseType: 'blob' as 'json',
+    });
   }
 
   // Método para obtener la foto antigua del usuario
   mirarFotoAntigua(perCodigo: String): Observable<Foto> {
     return this.http.get<Foto>(
-      `${this.url}/obtener-foto-antigua/${this.userLogeado}/${perCodigo}`,
+      `${this.url}/obtener-foto-antigua/${perCodigo}`,
       { headers: this.aggAutorizacionHeader() }
     );
   }
